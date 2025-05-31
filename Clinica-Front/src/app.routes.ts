cee299e187eaf2a149fb77c88d6eacd866e8e85f
RafaelSales93 <fco.rsales@gmail.com>
2025-05-31 11:05:16 -0300

    add medico

--- Clinica-Front/src/app.routes.ts
+++ Clinica-Front/src/app.routes.ts
@@ -5,13 +5,19 @@ import { HomePage } from './app/clinica/home/home.component';
 import { ListagemComponent } from './app/clinica/telaPaciente/paciente/listagemPaciente/listagemPaciente.component';
 import {HistoricoPacienteComponent} from './app/clinica/telaHistorico/historico-paciente/historico-paciente.component';
 import { historicoResolver } from './app/clinica/telaHistorico/historico-paciente/historico.resolver';
+import { MedicoComponent } from './app/clinica/telaMedico/medico/medico.component';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'home', component: HomePage },
   { path: 'paciente', component: PacienteComponent },
+  { path: 'medico', component: MedicoComponent},
   { path: 'consulta', component: ConsultaComponent },
   { path: 'listagem', component: ListagemComponent },
+
+  { path: 'paciente/:id',loadComponent: () => import('./app/clinica/telaPaciente/paciente/paciente.component').then(m => m.PacienteComponent) },  
+  { path: '**', redirectTo: '' },
+
   { 
     path: 'historico',
     component: HistoricoPacienteComponent,
@@ -19,6 +25,4 @@ export const routes: Routes = [
       historico: historicoResolver
     }
   },
-  { path: 'paciente/:id',loadComponent: () => import('./app/clinica/telaPaciente/paciente/paciente.component').then(m => m.PacienteComponent) },  
-  { path: '**', redirectTo: '' },
 ];
